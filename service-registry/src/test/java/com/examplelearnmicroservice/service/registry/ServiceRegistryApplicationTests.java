36073143ab86226e79f16ae2fd93c18f4fecdf35
sinhatanmay18 <sinha.t18@outlook.com>
2023-08-29 00:39:43 +0530

    first commit

---
+++ service-registry/src/test/java/com/examplelearnmicroservice/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplelearnmicroservice.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
